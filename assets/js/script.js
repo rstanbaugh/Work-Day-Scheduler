0280d4bb7dac42db70d9b7931508eff886286074
rstanbaugh <rich_bootcamp@icloud.com>
2022-05-06 19:43:31 -0400

    some local storage done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,88 +2,121 @@
 $('#currentDay').text(moment().format('dddd, MMM Do'));
 
 // tasks array
-tasks = {};
+wdsTasks = [];
 
 // function to update teh formatting of the tasks based on time
 var auditTasks = function(tstHour) {
-// get current hour
-
-// tstHour is for testing hte auditTasks function
-var currentHour;
-if(tstHour){
-  currentHour = tstHour;
-} else {
-  currentHour = moment().format("H");
-}
-
-
-// loop through each time block
-var taskEl = $(".taskDiv");
-
-taskEl.each(function(i){
-  var taskHour = $(taskEl[i]).data("hour");
-  // remove all classes: "past" "present" and "future"
-  $(taskEl[i]).removeClass("past")
-  $(taskEl[i]).removeClass("present")
-  $(taskEl[i]).removeClass("future")
-
-  if (currentHour == taskHour ){        /* if task hour is now */
-    $(taskEl[i]).addClass("present")  
-  } else if (currentHour > taskHour){   /* if task hour is before now*/
-    $(taskEl[i]).addClass("past")
-  } else {                              /* task hour imust be after now */
-    $(taskEl[i]).addClass("future")
+  // get current hour
+  // tstHour is for testing hte auditTasks function
+  var currentHour;
+  if(tstHour){
+    currentHour = tstHour;
+  } else {
+    currentHour = moment().format("H");
   }
-});
 
+  // select all time blocks and loop through each
+  var taskEl = $(".taskDiv");
+  taskEl.each(function(i){
+    var taskHour = $(taskEl[i]).data("hour");
+    // remove all classes: "past" "present" and "future"
+    $(taskEl[i]).removeClass("past")
+    $(taskEl[i]).removeClass("present")
+    $(taskEl[i]).removeClass("future")
 
-
-// set timer automation
-setInterval(function(){
-    auditTask();
-}, (1000*60)*10);
-
+    if (currentHour == taskHour ){        /* if task hour is now */
+      $(taskEl[i]).addClass("present")  
+    } else if (currentHour > taskHour){   /* if task hour is before now*/
+      $(taskEl[i]).addClass("past")
+    } else {                              /* task hour imust be after now */
+      $(taskEl[i]).addClass("future")
+    }
+  });
 };
 
-$(".saveBtn").on("click",function(){
-  console.log("save button clicked");
-});
+auditTasks();
+// set timer automation to update formatting every 10 minutes
+setInterval(function(){
+  auditTasks();
+}, (1000*60)*10);
 
 
-// $(".task").on("click","textarea", function(){
+// click task to edit code
 $(".taskDiv").on("click", "p", function(){
   // get current text of p element
   var text = $(this)
     .text()
-    .trim();
+    .trim()
+  
+  // save the data attribute 
+  // var dataHour = num.toString($(this).parents().data("hour"));
+  var dataHour = (($(this).parents().data("hour").toString()));
+
 
   // replace p element with a new textarea
   var textInput = $("<textarea>")
     .addClass("form-control")
     .val(text);
+    textInput.attr("id",dataHour)   /* pass the data attribute along */
+
   $(this).replaceWith(textInput);
 
   // auto focus new element
   textInput.trigger("focus");
 });
 
-  // editable field was un-focused
-  $(".taskDiv").on("blur", "textarea", function() {
-    //get the textareas; current value/text
-      var text = $(this)
-        .val()
-        .trim();
-  
-      //recreate p element
-      var taskP = $("<p>")
-        .addClass("m-1")
-        .text(text);
-  
-      // replace textarea with new content
-      $(this).replaceWith(taskP);
-    }); 
+// save edits when focus changes from editable field
+$(".taskDiv").on("blur", "textarea", function() {
+  //get the textareas; current value/text
+    var text = $(this)
+      .val()
+      .trim();
+
+    //recreate p element
+    var taskP = $("<p>")
+      .addClass("m-1")
+      .text(text);
+
+    // extract the taskHour that was passed in data-hour
+    var taskHour = $($(this).parents()[0]).data("hour");
+    // console.log('taskHour',taskHour);
+    console.log(taskHour,text);
+
+    // replace textarea with new content
+    $(this).replaceWith(taskP);
+
+    // update task array wdsTasks
+    if(wdsTasks.length>0){
+      // check for dups
+      debugger
+      for (i=0;i<wdsTasks.length;i++){
+        if(wdsTasks[i].hour === taskHour){
+          // there is a dup
+          wdsTasks[i].task = text;
+        } else {
+          // there is not a dup; add a new object
+          wdsTasks.push({
+            hour: taskHour,
+            task: text
+          });
+          break;
+        }
+    } else {
+      // add a new object
+      wdsTasks.push({
+        hour: taskHour,
+        task: text
+      });
+    }
+
+  }); 
 
 
+// 
+
+// $(".saveBtn").on("click",function(){
+//   console.log("save button clicked");
+// });
 
 var loadTasks = function(){
   // load from local storage
@@ -108,5 +141,3 @@ var loadTasks = function(){
 var saveTasks = function(){
 // save to local storage
 }
-
-auditTasks();
